79143625c40aa6dfa90dc0b778ccb191d47ec49c
Sai Thiha Zaw <sai.t.zaw@frontiir.net>
2022-09-08 23:48:42 +0630

    union to join same schema

---
+++ SparkCourses/src/main/scala/DataAnalysis/MergedAsiaEuropeData.scala
@@ -0,0 +1,41 @@
+package DataAnalysis
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.col
+
+object MergedAsiaEuropeData {
+  def csvFilePath(csvFileName: String): String = {
+    val filePath = "product/"
+    val csvFile = filePath + csvFileName
+    csvFile
+  }
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .appName("AsiaEuropeDataApp")
+      .master("local")
+      .getOrCreate()
+    val csvFileName = csvFilePath("5mSaleRecordNormalized.csv")
+    val csvFileToAnalyze = spark.read
+      .option("header", value = true)
+      .option("inferSchema", value = true)
+      .csv(csvFileName)
+
+    val AsianSaleRecordDF = csvFileToAnalyze.filter("region == 'Asia'")
+    println(
+      "total number of row count in Asian region: " + AsianSaleRecordDF.count()
+    )
+
+    val EuropeSaleRecordDF = csvFileToAnalyze.filter("region == 'Europe'")
+    println(
+      "total number of row count in Europe region: " + EuropeSaleRecordDF
+        .count()
+    )
+
+    val mergedAsiaEuropeData = AsianSaleRecordDF.union(EuropeSaleRecordDF)
+
+    mergedAsiaEuropeData.show()
+    println("total row count: " + mergedAsiaEuropeData.count())
+
+  }
+}
